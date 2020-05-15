28932c31b21e18ab613df91d777900f1e2091009
dvega1193 <dvega1193@gmail.com>
2020-05-14 20:24:09 -0600

    actualizacion

--- app/navigations/Navigation.js
+++ app/navigations/Navigation.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
+import { Icon } from "react-native-elements";
 import RestaurantsStack from "../navigations/RestaurantsStack";
 import FavoritesStack from "../navigations/FavoritesStack";
 import TopRestaurantsStack from "../navigations/TopRestaurantsStack";
@@ -13,7 +13,20 @@ const Tab = createBottomTabNavigator();
 export default function Navigation() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        initialRouteName="restaurants" //la primera pagina q carga el navegador al entrar al app
+        tabBarOptions={{
+          //color del tab cuando no esta seleccionado
+          inactiveTintColor: "#646464",
+          //color del tab cuando esta seleccionado
+          activeTintColor: "#00a680",
+        }}
+        screenOptions={({ route }) => ({
+          //optiene la ruta del q esta seleccionado
+          //obtiene el color dependiendo si esta seleccionado o no
+          tabBarIcon: ({ color }) => screenOptions(route, color), //envia los datos a la funcion
+        })}
+      >
         <Tab.Screen
           name="restaurants"
           component={RestaurantsStack} //se llama el stak y el stack se agrega la screen ademas de la barra de arriba con el nombre de la pagina
@@ -41,5 +54,34 @@ export default function Navigation() {
         />
       </Tab.Navigator>
     </NavigationContainer>
+  );
+}
+
+function screenOptions(route, color) {
+  let iconName;
+
+  //segun el nomnbre del tan se le asigna el icono el tamaño y el color
+  switch (route.name) {
+    case "restaurants":
+      iconName = "compass-outline";
+      break;
+    case "favorites":
+      iconName = "heart-outline";
+      break;
+    case "top-restaurants":
+      iconName = "star-outline";
+      break;
+    case "search":
+      iconName = "magnify";
+      break;
+    case "account":
+      iconName = "home-outline";
+      break;
+
+    default:
+      break;
+  }
+  return (
+    <Icon type="material-community" name={iconName} size={22} color={color} /> //se le inserta el icono al tab del menu
   );
 }
---
+++ app/utils/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDxQY0GAcTO2C102gM8vHzDSLN7-DAt4F0",
+  authDomain: "tenedores-7ee58.firebaseapp.com",
+  databaseURL: "https://tenedores-7ee58.firebaseio.com",
+  projectId: "tenedores-7ee58",
+  storageBucket: "tenedores-7ee58.appspot.com",
+  messagingSenderId: "918784528965",
+  appId: "1:918784528965:web:24ed5f39f6a249d4e4092c",
+};
+
+export const firebaseapp = firebase.initializeApp(firebaseConfig);
