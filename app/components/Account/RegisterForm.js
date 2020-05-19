0bc2b8bebc8c5bcdcc1d794e4a32d1e6b987cd5a
dvega1193 <dvega1193@gmail.com>
2020-05-19 09:29:26 -0600

    nueva funcion de mostrar y ocultar contraseñas

--- app/components/Account/RegisterForm.js
+++ app/components/Account/RegisterForm.js
@@ -1,24 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
 import { Input, Icon, Button } from "react-native-elements";
 export default function RegisterForm() {
+  //estados para mostrar y ocultar las contraseñas
+  const [showPassword, setShowPassword] = useState(false);
+  const [showRepeatPassword, setShowRepeatPassword] = useState(false);
   return (
     <View style={styles.formContainer}>
       <Input
         placeholder="Correo electronico"
         containerStyle={styles.inputform}
+        rightIcon={
+          <Icon
+            type="material-community"
+            name="at"
+            iconStyle={styles.iconRight}
+          />
+        }
       />
       <Input
         placeholder="Contaseña"
         containerStyle={styles.inputform}
         password={true}
-        secureTextEntry={true}
+        secureTextEntry={showPassword ? false : true}
+        rightIcon={
+          <Icon
+            type="material-community"
+            name={showPassword ? "eye-off-outline" : "eye-outline"}
+            iconStyle={styles.iconRight}
+            onPress={() => setShowPassword(!showPassword)} //se envia el valor contrario que tiene showPassword para eso es el !
+          />
+        }
       />
       <Input
         placeholder="Repetir contaseña"
         containerStyle={styles.inputform}
         password={true}
-        secureTextEntry={true}
+        secureTextEntry={showRepeatPassword ? false : true}
+        rightIcon={
+          <Icon
+            type="material-community"
+            name="eye-outline"
+            iconStyle={styles.iconRight}
+            onPress={() => setShowRepeatPassword(!showRepeatPassword)} //se envia el valor contrario que tiene showPassword para eso es el !
+          />
+        }
       />
       <Button
         title="Unirse"
@@ -46,5 +72,8 @@ const styles = StyleSheet.create({
   },
   btnRegister: {
     backgroundColor: "#00a680",
+  },
+  iconRight: {
+    color: "#c1c1c1",
   },
 });
